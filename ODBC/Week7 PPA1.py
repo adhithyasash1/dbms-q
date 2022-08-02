6168f1c87e759d5ebe36c9cf176f578226d6c1c9
Sashi Adhithya <70097892+adhithyasash1@users.noreply.github.com>
2022-08-02 08:04:40 +0530

    Update Week7 PPA1.py

--- ODBC/Week7 PPA1.py
+++ ODBC/Week7 PPA1.py
@@ -1,3 +1,20 @@
+'''
+In this question, you have to write a Python program to print the names of the players and the team of each player of all those players whose jersey number is a prime number. 
+
+The list should be ordered in reverse alphabetical order of player names. If two or more players have the same name, then further sorting should be done on the team name, again in reverse alphabetical order.
+
+The format of output is as given below:
+
+Name of the player, followed by a comma (,), then a space and then the team name.
+
+
+For example, if Arjun has jersey number 5 and is playing for All Stars and Pranav, with jersey number 7, is playing for team Amigos, then the output will be:
+
+Pranav, Amigos
+
+Arjun, All Stars
+'''
+
 import psycopg2
 import sys
 import os
