9dd3a7eecf7b622ef797e49ac19f999134ce1379
Sashi Adhithya <70097892+adhithyasash1@users.noreply.github.com>
2022-08-02 08:02:19 +0530

    Update Week7 GrPa1.py

--- ODBC/Week7 GrPa1.py
+++ ODBC/Week7 GrPa1.py
@@ -1,3 +1,14 @@
+'''
+In this question, you must write a Python program to output the name of the main referee for a given match date (in yyyy-mm-dd format). The input to your program is a file named “date.txt” that has the match date as the first word of the file. Your program must assume that date.txt resides in the same folder as your Python program. 
+
+
+The output name has to be formatted as follows. The last name is displayed followed by the initials of the first name, then a full stop, a space and then the initials of the middle name (if the middle name exists), followed by a full stop.
+
+For example, if the name of the main referee is “Kennedy Sapam”, the output must be ”Sapam K.” 
+
+If the name of the main referee is “Asit Kumar Sarkar”, the output must be ”Sarkar A. K.”
+'''
+
 week7 grpa1
 
 import psycopg2
