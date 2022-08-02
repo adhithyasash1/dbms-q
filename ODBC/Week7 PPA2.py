b60493b858f7b58d222951b5e01d62cd9b01d24d
Sashi Adhithya <70097892+adhithyasash1@users.noreply.github.com>
2022-08-02 08:05:09 +0530

    Update Week7 PPA2.py

--- ODBC/Week7 PPA2.py
+++ ODBC/Week7 PPA2.py
@@ -1,3 +1,22 @@
+'''
+In this problem, you have to write a Python program to print an encoding of the ids of the host teams whose jersey colour at home is different from the jersey colour when they play away from home.
+
+
+The encoding must be using a shift cipher, which is detailed below.
+
+An alphabet is mapped to another alphabet as follows. For a given alphabet α, let pos be the position at which α occurs in the alphabet listing (A at 1, B at 2, …. Z at 26). Then the encoding of α is the alphabet at the position (pos + 7) mod 26.
+
+For example, if M is the alphabet, then the position at which M occurs in the alphabet listing is 13. Then, the encoding of M is the alphabet at the position (13 + 7) mod 26 = 20, which is T. 
+
+For each digit β, the encoding of β is (β+7) mod 10.
+
+For example, if 3 is the digit, then the encoding of 3 is the number (3 + 7) mod 10 = 0.
+
+The ids should be listed in the ascending order before performing the encoding.
+
+Each line in the output of the program must correspond to one row retrieved from the table.
+'''
+
 import psycopg2 as pg
 import sys
 import os
